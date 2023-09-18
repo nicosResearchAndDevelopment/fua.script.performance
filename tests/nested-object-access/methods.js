769e2a7d89d81c0b47f5f04ba777adbb098bbcaf
Simon Petrac <pfoerdie@gmail.com>
2023-09-18 15:26:19 +0200

    added comparison for flat and nested object access

---
+++ tests/nested-object-access/methods.js
@@ -0,0 +1,43 @@
+const
+    keyParts     = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqr', 'stu', 'vwx', 'yz0', '123', '456', '789'],
+    keyVariants  = [],
+    flatObject   = {},
+    nestedObject = {};
+
+for (let first of keyParts) {
+    nestedObject[first] = {};
+    for (let second of keyParts) {
+        nestedObject[first][second] = {};
+        for (let third of keyParts) {
+            nestedObject[first][second][third] = {};
+            for (let fourth of keyParts) {
+                nestedObject[first][second][third][fourth] = {};
+                for (let fifth of keyParts) {
+                    const combined = first + second + third + fourth + fifth;
+                    keyVariants.push([first, second, third, fourth, fifth]);
+                    flatObject[combined]                              = combined;
+                    nestedObject[first][second][third][fourth][fifth] = combined;
+                }
+            }
+        }
+    }
+}
+
+exports._nestingDepth = 5;
+exports._keyParts     = keyParts.length;
+exports._keyVariants  = keyVariants.length;
+
+exports._randomKeyVariant = function () {
+    const randomIndex = Math.floor(Math.random() * keyVariants.length);
+    return keyVariants[randomIndex];
+};
+
+exports.accessFlatObject = function ([first, second, third, fourth, fifth]) {
+    const combined = first + second + third + fourth + fifth;
+    return flatObject[combined] === combined;
+};
+
+exports.accessNestedObject = function ([first, second, third, fourth, fifth]) {
+    const combined = first + second + third + fourth + fifth;
+    return nestedObject[first][second][third][fourth][fifth] === combined;
+};
---
+++ tests/nested-object-access/performance.js
@@ -0,0 +1,20 @@
+const
+    methods          = require('./methods.js'),
+    methodNames      = Object.keys(methods).filter(key => !key.startsWith('_')),
+    {Test, Runtime}  = require('../../src/model.js'),
+    methodComparison = new Runtime(methods._randomKeyVariant),
+    testRepetitions  = 5000,
+    sampleSize       = 2000;
+
+console.log('nestingDepth:', methods._nestingDepth);
+console.log('keyParts:', methods._keyParts);
+console.log('keyVariants:', methods._keyVariants);
+console.log('testRepetitions:', testRepetitions);
+console.log('sampleSize:', sampleSize);
+
+for (let name of methodNames) {
+    const test = new Test(name, methods[name]);
+    methodComparison.register(test);
+}
+
+methodComparison.exec(testRepetitions, sampleSize).print();
