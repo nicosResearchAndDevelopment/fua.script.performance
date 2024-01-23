31deafb9c568bb9c4dacc17cd820b5c2c478a7c9
Simon Petrac <pfoerdie@gmail.com>
2024-01-23 13:15:03 +0100

    fetch performance

---
+++ tests/fetch/methods.js
@@ -0,0 +1,53 @@
+const
+    http                  = require('http'),
+    node_fetch            = require('node-fetch'),
+    {fetch: undici_fetch} = require('undici'),
+    HTTP                  = require('@nrd/fua.client.http'),
+    client                = HTTP();
+
+Object.defineProperties(exports, {
+    port:  {
+        enumerable: false,
+        value:      3000
+    },
+    start: {
+        enumerable: false,
+        value:      async function () {
+            this.server = http.createServer((request, response) => {
+                response.write(request.url);
+                response.end();
+            });
+            await new Promise(resolve => this.server.listen(this.port, resolve));
+        }
+    },
+    stop:  {
+        enumerable: false,
+        value:      async function () {
+            await new Promise(resolve => this.server.close(resolve));
+        }
+    }
+});
+
+exports['no-fetch'] = async function (param) {
+    return param.url;
+};
+
+exports['native-fetch'] = async function (param) {
+    const response = await fetch(param.url, param);
+    return await response.text();
+};
+
+exports['node-fetch'] = async function (param) {
+    const response = await node_fetch(param.url, param);
+    return await response.text();
+};
+
+exports['undici-fetch'] = async function (param) {
+    const response = await undici_fetch(param.url, param);
+    return await response.text();
+};
+
+exports['client-fetch'] = async function (param) {
+    const response = await client.fetch(param.url, param);
+    return await response.text();
+};
---
+++ tests/fetch/performance.js
@@ -0,0 +1,24 @@
+const
+    methods                   = require('./methods.js'),
+    methodNames               = Object.keys(methods),
+    {AsyncTest, AsyncRuntime} = require('../../src/model.js'),
+    uuid                      = require('@nrd/fua.core.uuid'),
+    dataGenerator             = () => ({
+        url:         `http://localhost:${methods.port}/${uuid.v4()}/`,
+        method:      'GET',
+        mode:        'cors',
+        credentials: 'omit',
+        cache:       'no-store',
+        redirect:    'follow'
+    }),
+    methodComparison          = new AsyncRuntime(dataGenerator);
+
+for (let name of methodNames) {
+    const test = new AsyncTest(name, methods[name]);
+    methodComparison.register(test);
+}
+methods.start()
+    .then(() => methodComparison.exec(1000, 5))
+    .then(runtime => runtime.print())
+    .catch(console.error)
+    .finally(() => methods.stop());
